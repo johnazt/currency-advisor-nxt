b4af83449421d97cf8898155452fc0e1ef34aee3
John Asto <john.azt.dev@gmail.com>
2023-02-12 12:18:51 -0300

    fix api

--- hooks/useData.js
+++ hooks/useData.js
@@ -6,7 +6,7 @@ const useData = () => {
 
 	useEffect(() => {
 		async function getData() {
-			const res = await fetch(`http://localhost:3001/api/indicadores`);
+			const res = await fetch(`https://mindicador.cl/api/`);
 			const data = await res.json();
 			setDataAPI(data);
 		}
